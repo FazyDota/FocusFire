8c2dfee2abd9c5c95a82765b34f819433ec99d05
FazyDota <Fazy64+dota@gmail.com>
2022-05-10 15:54:41 +0200

    Improving performance by only saving images in DEBUG mode

--- main.py
+++ main.py
@@ -87,7 +87,8 @@ def handle_draft_sector_parsing(draft_screenshot):
         if sector_idx == 0:
             green_mask = cv2.inRange(input_image, (20, 120, 20), (70, 255, 70))
             extracted_text = OCR_text_from_image(255 - green_mask)
-            cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_2_green.png', 255 - green_mask)
+            if DEBUG:
+                cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_2_green.png', 255 - green_mask)
             logging.debug(f"Trying to extract from sector {sector_idx} via green mask, result: {extracted_text}")
 
             if validated_extracted_text(extracted_text):
@@ -97,7 +98,8 @@ def handle_draft_sector_parsing(draft_screenshot):
 
             red_mask = cv2.inRange(input_image, (21, 30, 145), (35, 60, 205))
             extracted_text = OCR_text_from_image(255 - red_mask)
-            cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_3_red.png', 255 - red_mask)
+            if DEBUG:
+                cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_3_red.png', 255 - red_mask)
             logging.debug(f"Trying to extract from sector {sector_idx} via red mask, result: {extracted_text}")
 
             if validated_extracted_text(extracted_text):
@@ -106,8 +108,9 @@ def handle_draft_sector_parsing(draft_screenshot):
                 return extracted_text
 
             extracted_text = OCR_text_from_image(255 - input_image)
-            cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_4_white_hsv.png', 255 - input_image)
-            cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_4_white.png', 255 - input_image)
+            if DEBUG:
+                cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_4_white_hsv.png', 255 - input_image)
+                cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_4_white.png', 255 - input_image)
             logging.debug(f"Trying to extract from sector {sector_idx} via white mask, result: {extracted_text}")
 
             if validated_extracted_text(extracted_text):
@@ -120,7 +123,8 @@ def handle_draft_sector_parsing(draft_screenshot):
             if (sector_idx < 5 and is_radiant) or (not is_radiant and sector_idx > 4):
                 green_mask = cv2.inRange(input_image, (20, 120, 20), (70, 255, 70))
                 extracted_text = OCR_text_from_image(255 - green_mask)
-                cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_green.png', 255 - green_mask)
+                if DEBUG:
+                    cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_green.png', 255 - green_mask)
                 logging.debug(f"Trying to extract from sector {sector_idx} via green mask, result: {extracted_text}")
 
                 if extracted_text != 'Bn' and extracted_text != 'Be' and len(extracted_text) > 2:
@@ -128,7 +132,8 @@ def handle_draft_sector_parsing(draft_screenshot):
                     return extracted_text
 
                 extracted_text = OCR_text_from_image(255 - input_image)
-                cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_white.png', 255 - input_image)
+                if DEBUG:
+                    cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_white.png', 255 - input_image)
                 logging.debug(f"Trying to extract from sector {sector_idx} via white mask, result: {extracted_text}")
 
                 if validated_extracted_text(extracted_text):
@@ -140,7 +145,8 @@ def handle_draft_sector_parsing(draft_screenshot):
             else:
                 red_mask = cv2.inRange(input_image, (21, 30, 145), (35, 60, 205))
                 extracted_text = OCR_text_from_image(255 - red_mask)
-                cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_2_red.png', 255 - red_mask)
+                if DEBUG:
+                    cv2.imwrite(f'sectors\\{date_string}_sector_{sector_idx}_2_red.png', 255 - red_mask)
                 logging.debug(f"Trying to extract from sector {sector_idx} via red mask, result: {extracted_text}")
 
                 if validated_extracted_text(extracted_text):
@@ -156,9 +162,11 @@ def handle_draft_sector_parsing(draft_screenshot):
     for idx, hero_name_sector in enumerate(hero_sectors):
         t1_start = process_time()
         logging.info(f'{idx} ')
-        cv2.imwrite(f'sectors\\{date_string}_sector_{idx}_0_raw.png', hero_name_sector)
+        if DEBUG:
+            cv2.imwrite(f'sectors\\{date_string}_sector_{idx}_0_raw.png', hero_name_sector)
         processed_image = cv2.GaussianBlur(hero_name_sector, (5, 5), 1)
-        cv2.imwrite(f'sectors\\{date_string}_sector_{idx}_1_gaussian.png', processed_image)
+        if DEBUG:
+            cv2.imwrite(f'sectors\\{date_string}_sector_{idx}_1_gaussian.png', processed_image)
         t1_stop = process_time()
         processing_time_total += t1_stop - t1_start
 
